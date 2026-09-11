117c0767343c72cc2ba6617a14227abeff27ef2a
Songlet Li <laoxing201314@outlook.com>
2026-09-11 15:55:40 +0800

    Create uci-defaults script for initial WiFi setup
    Add a script to configure default LAN IP and WiFi settings on first boot.

--- customize_immortalwrt.sh
+++ customize_immortalwrt.sh
@@ -14,3 +14,38 @@ sed -i 's/192.168.1.1/192.168.16.1/g' /builder/openwrt/package/base-files/files/
 
 #3. Replace with JerryKuKu’s Argon
 #rm /builder/openwrt/package/lean/luci-theme-argon -rf
+
+#4. Modify hostname
+sed -i 's/OpenWrt/Redmi-Wrt/g' package/base-files/files/bin/config_generate
+
+# ========== 新增：生成uci-defaults 首次开机自动配置 LAN IP + WiFi ==========
+# 源码内files目录，编译时打包进固件
+mkdir -p /builder/openwrt/files/etc/uci-defaults
+
+cat > /builder/openwrt/files/etc/uci-defaults/99-set-default-wifi-ip <<'EOF'
+#!/bin/sh
+
+# 设置LAN IP
+# uci set network.lan.ipaddr='192.168.16.1'
+# uci set network.lan.netmask='255.255.255.0'
+
+# 2.4G radio0
+uci set wireless.radio0.disabled='0'
+uci set wireless.radio0.country='CN'
+uci set wireless.default_radio0.ssid='Redmi-2.4G'
+uci set wireless.default_radio0.encryption='psk2+ccmp'
+uci set wireless.default_radio0.key='qwer1234'
+
+# 5G radio1
+uci set wireless.radio1.disabled='0'
+uci set wireless.radio1.country='CN'
+uci set wireless.default_radio1.ssid='Redmi-5G'
+uci set wireless.default_radio1.encryption='psk2+ccmp'
+uci set wireless.default_radio1.key='qwer1234'
+
+uci commit network
+uci commit wireless
+exit 0
+EOF
+
+chmod +x /builder/openwrt/files/etc/uci-defaults/99-set-default-wifi-ip
